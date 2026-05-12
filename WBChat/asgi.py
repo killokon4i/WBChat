3231d4f61d6395d6b89ad6c7a160ab0e320a16a2
killokon4i <ultramegalutiybrawler228@gmail.com>
2026-05-13 00:58:20 +0300

    Update asgi.py

--- WBChat/asgi.py
+++ WBChat/asgi.py
@@ -1,19 +1,16 @@
 import os
-import django
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WBChat.settings')
-django.setup()  # Важно: инициализировать Django
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WBChat.settings')
+
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
